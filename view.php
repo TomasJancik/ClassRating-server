6713c59be9a6ba720297d0ef6b773c70450b7d71
Tomas Jancik <t.jancik@gmail.com>
2019-11-14 21:55:10 +0100

    add ability to set date

--- view.php
+++ view.php
@@ -1,15 +1,18 @@
 <?php
 
-$dateYesterday = date("Y-m-d", time() - 60*60*24);
-$dateToday = date("Y-m-d");
+if(isset($_GET['date']) && preg_match("/[0-9]{4}-[0-9]{2}-[0-9]{2}/", $_GET['date'])) {
+	$date = $_GET['date'];
+} else {
+	$date = date("Y-m-d");
+}
 
 $query = "SELECT distinct date(date) as day, SEC_TO_TIME(FLOOR((TIME_TO_SEC(date)+300)/600)*600) as `time`, rating, count(SEC_TO_TIME(FLOOR((TIME_TO_SEC(date)+300)/600)*600)) as `count`" .
 		"from data " .
-		"where date >= ? " .
+		"where date(date) = ? " .
 		"group by SEC_TO_TIME(FLOOR((TIME_TO_SEC(date)+300)/600)*600), rating";
 
 if($stmt = $db->prepare($query)) {
-	$stmt->bind_param("s", $dateYesterday);
+	$stmt->bind_param("s", $date);
 	$stmt->execute();
 	$res = $stmt->get_result();
 } else {
@@ -19,18 +22,12 @@ if($stmt = $db->prepare($query)) {
 $data = array();
 
 while($row = $res->fetch_assoc()) {
-	if(!isset($data[$row['day']])) {
-		$data[$row['day']] = array();
+	if(!isset($data[$row['time']])) {
+		$data[$row['time']] = array(1 => 0, 2 => 0, 3 => 0);
 	}
 
-	if(!isset($data[$row['day']][$row['time']])) {
-		$data[$row['day']][$row['time']] = array(1 => 0, 2 => 0, 3 => 0);
-	}
-
-	$data[$row['day']][$row['time']][$row['rating']] = $row['count'];
+	$data[$row['time']][$row['rating']] = $row['count'];
 }
-
-$data = isset($data[$dateToday]) ? $data[$dateToday] : $data[$dateYesterday];
 
 echo <<< xxx
 <doctype !HTML>
@@ -39,7 +36,11 @@ echo <<< xxx
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.2/dist/Chart.min.js"></script>
 </head>
 <body>
+<form name="crate-view">
 xxx;
+echo "\t<input type='date' id='date' name='date' value='" . $date . "'>\n";
+echo "\t<input type='submit' name='submit' value='Show me'>\n";
+echo "</form>";
 
 
 echo "<table border='1'><thead><tr><td>Time</td><td>😊</td><td>😐</td><td>😟</td></tr></thead>\n";
