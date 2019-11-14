5db5418fb748335e7574b7fa78b3f6e490e2f505
Tomas Jancik <t.jancik@gmail.com>
2019-11-14 22:22:00 +0100

    shaw last 7 day by default

--- view.php
+++ view.php
@@ -2,16 +2,19 @@
 
 if(isset($_GET['date']) && preg_match("/[0-9]{4}-[0-9]{2}-[0-9]{2}/", $_GET['date'])) {
 	$date = $_GET['date'];
+	$operator = "=";
 } else {
-	$date = date("Y-m-d");
+	$date = date("Y-m-d", time() - 7*24*60*60);
+	$operator = ">=";
 }
 
 $query = "SELECT distinct date(date) as day, SEC_TO_TIME(FLOOR((TIME_TO_SEC(date)+300)/600)*600) as `time`, rating, count(SEC_TO_TIME(FLOOR((TIME_TO_SEC(date)+300)/600)*600)) as `count`" .
 		"from data " .
-		"where date(date) = ? " .
-		"group by SEC_TO_TIME(FLOOR((TIME_TO_SEC(date)+300)/600)*600), rating";
+		"where date(date) #OPERATOR# ? " .
+		"group by date(date), SEC_TO_TIME(FLOOR((TIME_TO_SEC(date)+300)/600)*600), rating " .
+		"order by date(date) desc";
 
-if($stmt = $db->prepare($query)) {
+if($stmt = $db->prepare(str_replace("#OPERATOR#", $operator, $query))) {
 	$stmt->bind_param("s", $date);
 	$stmt->execute();
 	$res = $stmt->get_result();
@@ -22,11 +25,15 @@ if($stmt = $db->prepare($query)) {
 $data = array();
 
 while($row = $res->fetch_assoc()) {
-	if(!isset($data[$row['time']])) {
-		$data[$row['time']] = array(1 => 0, 2 => 0, 3 => 0);
+	if(!isset($data[$row['day']])) {
+		$data[$row['day']] = array();
 	}
 
-	$data[$row['time']][$row['rating']] = $row['count'];
+	if(!isset($data[$row['day']][$row['time']])) {
+		$data[$row['day']][$row['time']] = array(1 => 0, 2 => 0, 3 => 0);
+	}
+
+	$data[$row['day']][$row['time']][$row['rating']] = $row['count'];
 }
 
 echo <<< xxx
@@ -42,32 +49,38 @@ echo "\t<input type='date' id='date' name='date' value='" . $date . "'>\n";
 echo "\t<input type='submit' name='submit' value='Show me'>\n";
 echo "</form>";
 
-
-echo "<table border='1'><thead><tr><td>Time</td><td>😊</td><td>😐</td><td>😟</td></tr></thead>\n";
 $rowNum = 0;
-foreach ($data as $time => $values) {
-	$rowNum++;
-	echo "<tr>\n";
-	echo "<td>" . $time . "</td>\n";
-	echo "<td>" . $values[1] . "</td>\n";
-	echo "<td>" . $values[2] . "</td>\n";
-	echo "<td>" . $values[3] . "</td>\n";
-	echo "<td><canvas id='r" . $rowNum . "'></canvas>\n";
-	echo '<script type="text/javascript">' . "\n";
-	echo 'var ctx = document.getElementById("r' . $rowNum . '").getContext("2d")' . "\n";
-	echo 'var chart = new Chart(ctx, {' . "\n";
-	echo "\t" . 'type: "pie",' . "\n";
-	echo "\t" . 'data: {labels: ["😊", "😐", "😟"],' . "\n";
-	echo "\t\t" . 'datasets: [{' . "\n";
-	echo "\t\t\t" . 'data: [' . $values[1] . ', ' . $values[2] . ', ' . $values[3] . '],' . "\n";
-	echo "\t\t\t" . 'backgroundColor: ["rgba(80, 220, 100, 1)", "rgba(248, 222, 126, 1)", "rgba(255, 8, 0, 1)"],' . "\n";
-	echo "\t}]"; // end of dataset
+foreach ($data as $day => $dayData) {
 
-	echo "}})\n";
-	echo "</script>\n";
+	echo "<h2>" . $day . "</h2>";
+	echo "<table border='1'><thead><tr><td>Time</td><td>😊</td><td>😐</td><td>😟</td></tr></thead>\n";
 
-	echo "</td>\n";
-	echo "</tr>\n";
+	foreach ($dayData as $time => $values) {
+		$rowNum++;
+		echo "<tr>\n";
+		echo "<td>" . $time . "</td>\n";
+		echo "<td>" . $values[1] . "</td>\n";
+		echo "<td>" . $values[2] . "</td>\n";
+		echo "<td>" . $values[3] . "</td>\n";
+		echo "<td><canvas id='r" . $rowNum . "'></canvas>\n";
+		echo '<script type="text/javascript">' . "\n";
+		echo 'var ctx = document.getElementById("r' . $rowNum . '").getContext("2d")' . "\n";
+		echo 'var chart = new Chart(ctx, {' . "\n";
+		echo "\t" . 'type: "pie",' . "\n";
+		echo "\t" . 'data: {labels: ["😊", "😐", "😟"],' . "\n";
+		echo "\t\t" . 'datasets: [{' . "\n";
+		echo "\t\t\t" . 'data: [' . $values[1] . ', ' . $values[2] . ', ' . $values[3] . '],' . "\n";
+		echo "\t\t\t" . 'backgroundColor: ["rgba(80, 220, 100, 1)", "rgba(248, 222, 126, 1)", "rgba(255, 8, 0, 1)"],' . "\n";
+		echo "\t}]"; // end of dataset
+
+		echo "}})\n";
+		echo "</script>\n";
+
+		echo "</td>\n";
+		echo "</tr>\n";
+	}
+
+	echo "</table>";
 }
 
 echo <<< xxx
